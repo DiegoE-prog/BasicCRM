aca416bc2862ca98f7966b8a8fa70585891a4824
DiegoE-prog <s16120278@alumnos.itsur.edu.mx>
2023-05-10 12:46:30 -0600

    Remove service registration in Program.cs and added DataServiceRegistration in the data layer for services registration.

--- Backend/BasicCRM/BasicCRM.API/Program.cs
+++ Backend/BasicCRM/BasicCRM.API/Program.cs
@@ -1,10 +1,4 @@
 using BasicCRM.Data;
-using BasicCRM.Data.Entities;
-using BasicCRM.Data.Repository.Interfaces;
-using BasicCRM.Data.Repository;
-using Microsoft.Extensions.Configuration;
-using BasicCRM.Business.Services.Interfaces;
-using BasicCRM.Business.Services;
 using BasicCRM.Business;
 using BasicCRM.API.Middleware;
 
@@ -13,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddBusinessServices();
+builder.Services.AddDataServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,9 +15,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
-
-builder.Services.AddScoped<IRepository<Address>, AddressRepository>();
-builder.Services.AddScoped<IRepository<Client>,  ClientRepository>();
 
 builder.Services.AddScoped<GlobalHandleExceptionMiddleware>();
 
---
+++ Backend/BasicCRM/BasicCRM.Data/DataServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using BasicCRM.Data.Entities;
+using BasicCRM.Data.Repository.Interfaces;
+using BasicCRM.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Configuration;
+
+namespace BasicCRM.Data
+{
+    public static class DataServiceRegistration
+    {
+        public static IServiceCollection AddDataServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository<Address>, AddressRepository>();
+            services.AddScoped<IRepository<Client>, ClientRepository>();
+
+            return services;
+        }
+    }
+}
