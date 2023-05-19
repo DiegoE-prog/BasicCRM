c1ecf3c055508ca64ea4fe1c694be68e80cec1af
DiegoE-prog <s16120278@alumnos.itsur.edu.mx>
2023-05-19 13:19:45 -0600

    Added a FormAddress component, all crud operartions works also the validations works

--- Frontend/basiccrm/src/Components/Address/AddAddress.jsx
+++ Frontend/basiccrm/src/Components/Address/AddAddress.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import { useState } from "react"
 import { addAddressAsync } from "../../Api/AddressApi"
 import { useNavigate } from "react-router-dom"
-import { useForm } from "react-hook-form"
+
 import Swal from "sweetalert2"
 import withReactContent from "sweetalert2-react-content"
+import FromAddress from "./FormAddress"
 
 function AddAddress() {
 	const [address, setAddress] = useState({
+		addressID: "",
 		addressLine: "",
 		addressDetails: "",
 		city: "",
@@ -16,24 +18,10 @@ function AddAddress() {
 		country: ""
 	})
 
-	const navigate = useNavigate()
 	const MySwal = withReactContent(Swal)
-	const {
-		handleSubmit,
-		register,
-		formState: { errors }
-	} = useForm()
+	const navigate = useNavigate()
 
-	const handleChange = (e) => {
-		console.log(errors)
-		const value = e.target.value
-		setAddress({
-			...address,
-			[e.target.name]: value
-		})
-	}
-
-	const OnSubmit = async () => {
+	const onSubmit = async () => {
 		const addressData = {
 			addressLine: address.addressLine,
 			addressDetails: address.addressDetails,
@@ -60,103 +48,7 @@ function AddAddress() {
 	return (
 		<div className="row p-2">
 			<div className="col-12 h4">Add new Address</div>
-			<form onSubmit={handleSubmit(OnSubmit)}>
-				<div className="col-12 mb-2 p-1">
-					<label htmlFor="addressLine" class="form-label">
-						Address Line
-					</label>
-					<input
-						type="text"
-						name="addressLine"
-						{...register("addressLine", {
-							required: "Required"
-						})}
-						className="form-control form-control-sm"
-						value={address.addressLine}
-						onChange={handleChange}
-					/>
-					<span className="text-danger">{errors.addressLine && errors.addressLine.message}</span>
-				</div>
-				<div className="col-12 mb-2  p-1">
-					<label htmlFor="addressDetails" class="form-label">
-						Address Details
-					</label>
-					<input type="text" name="addressDetails" className="form-control form-control-sm" value={address.addressDetails} onChange={handleChange} />
-				</div>
-				<div className="col-12 mb-2 p-1">
-					<label htmlFor="city" class="form-label">
-						City
-					</label>
-					<input
-						type="text"
-						name="city"
-						{...register("city", {
-							required: "Required"
-						})}
-						className="form-control form-control-sm"
-						value={address.city}
-						onChange={handleChange}
-					/>
-					<span className="text-danger">{errors.city && errors.city.message}</span>
-				</div>
-				<div className="col-12 mb-2 p-1">
-					<label htmlFor="state" class="form-label">
-						State
-					</label>
-					<input
-						type="text"
-						name="state"
-						{...register("state", {
-							required: "Required"
-						})}
-						className="form-control form-control-sm"
-						value={address.state}
-						onChange={handleChange}
-					/>
-					<span className="text-danger">{errors.state && errors.state.message}</span>
-				</div>
-				<div className="col-12 mb-2 p-1">
-					<label htmlFor="zipCode" class="form-label">
-						Zip Code
-					</label>
-					<input
-						type="number"
-						name="zipCode"
-						{...register("zipCode", {
-							required: "Zip Code is Required",
-							pattern: {
-								value: /^[0-9]{5}$/i,
-								message: "Zip code needs to be a 5 digit number"
-							}
-						})}
-						className="form-control form-control-sm"
-						value={address.zipCode}
-						onChange={handleChange}
-					/>
-					<span className="text-danger">{errors.zipCode && errors.zipCode?.message}</span>
-				</div>
-				<div className="col-12 mb-2 p-1">
-					<label htmlFor="country" class="form-label">
-						Country
-					</label>
-					<input
-						type="text"
-						name="country"
-						{...register("country", {
-							required: "Required"
-						})}
-						className="form-control form-control-sm"
-						onChange={handleChange}
-						value={address.country}
-					/>
-					<span className="text-danger">{errors.country && errors.country.message}</span>
-				</div>
-				<div className="col-12 col-md-6 offset-md-3 p-2">
-					<button type="submit" className="btn btn-primary btn-sm form-control">
-						Add
-					</button>
-				</div>
-			</form>
+			<FromAddress onSubmit={onSubmit} address={address} setAddress={setAddress} />
 		</div>
 	)
 }
--- Frontend/basiccrm/src/Components/Address/EditAddress.jsx
+++ Frontend/basiccrm/src/Components/Address/EditAddress.jsx
@@ -5,6 +5,7 @@ import { getAddressAsync, editAddressAsync } from "../../Api/AddressApi"
 import { useNavigate } from "react-router-dom"
 import Swal from "sweetalert2"
 import withReactContent from "sweetalert2-react-content"
+import FromAddress from "./FormAddress"
 
 function EditAddress() {
 	const { id } = useParams()
@@ -38,8 +39,7 @@ function EditAddress() {
 		})
 	}
 
-	const handleSubmit = async (e) => {
-		e.preventDefault()
+	const onSubmit = async () => {
 		const addressData = {
 			addressID: address.addressID,
 			addressLine: address.addressLine,
@@ -67,49 +67,7 @@ function EditAddress() {
 	return (
 		<div className="row p-2">
 			<div className="col-12 h4">Edit Address</div>
-			<form onSubmit={handleSubmit}>
-				<div className="col-12 mb-2 p-1">
-					<label htmlFor="addressLine" class="form-label">
-						Address Line
-					</label>
-					<input type="text" name="addressLine" className="form-control form-control-sm" value={address.addressLine} onChange={handleChange} />
-				</div>
-				<div className="col-12 mb-2  p-1">
-					<label htmlFor="addressDetails" class="form-label">
-						Address Line
-					</label>
-					<input type="text" name="addressDetails" className="form-control form-control-sm" value={address.addressDetails} onChange={handleChange} />
-				</div>
-				<div className="col-12 mb-2 p-1">
-					<label htmlFor="city" class="form-label">
-						City
-					</label>
-					<input type="text" name="city" className="form-control form-control-sm" value={address.city} onChange={handleChange} />
-				</div>
-				<div className="col-12 mb-2 p-1">
-					<label htmlFor="state" class="form-label">
-						State
-					</label>
-					<input type="text" name="state" className="form-control form-control-sm" value={address.state} onChange={handleChange} />
-				</div>
-				<div className="col-12 mb-2 p-1">
-					<label htmlFor="zipCode" class="form-label">
-						Zip Code
-					</label>
-					<input type="number" name="zipCode" className="form-control form-control-sm" value={address.zipCode} onChange={handleChange} />
-				</div>
-				<div className="col-12 mb-2 p-1">
-					<label htmlFor="country" class="form-label">
-						Country
-					</label>
-					<input type="text" name="country" className="form-control form-control-sm" value={address.country} onChange={handleChange} />
-				</div>
-				<div className="col-12 col-md-6 offset-md-3 p-2">
-					<button type="submit" className="btn btn-primary btn-sm form-control">
-						Edit
-					</button>
-				</div>
-			</form>
+			<FromAddress onSubmit={onSubmit} address={address} setAddress={setAddress} />
 		</div>
 	)
 }
