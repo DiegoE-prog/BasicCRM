1450c2f3c147c9816f7967c8e6bcaba2dead160d
DiegoE-prog <s16120278@alumnos.itsur.edu.mx>
2023-05-29 12:37:00 -0600

    Added environment check to react

--- Frontend/basiccrm/src/Api/AddressApi.js
+++ Frontend/basiccrm/src/Api/AddressApi.js
@@ -1,27 +1,29 @@
 import axios from "axios"
 
+const URL = process.env.NODE_ENV === "development" ? "https://localhost:7081" : "dev-crmbasic-wepapi.azurewebsites.net"
+
 const getAdressesAsync = async () => {
-	const response = await axios.get("https://localhost:7081/api/Address/")
+	const response = await axios.get(`${URL}/api/Address/`)
 	return response
 }
 
 const getAddressAsync = async (id) => {
-	const response = await axios.get(`https://localhost:7081/api/Address/${id}`)
+	const response = await axios.get(`${URL}/api/Address/${id}`)
 	return response
 }
 
 const addAddressAsync = async (address) => {
-	const response = await axios.post("https://localhost:7081/api/Address/", address)
+	const response = await axios.post(`${URL}/api/Address/`, address)
 	return response
 }
 
 const editAddressAsync = async (address) => {
-	const response = await axios.put("https://localhost:7081/api/Address/", address)
+	const response = await axios.put(`${URL}/api/Address/`, address)
 	return response
 }
 
 const deleteAddressAsync = async (id) => {
-	const response = await axios.delete(`https://localhost:7081/api/Address/${id}`)
+	const response = await axios.delete(`${URL}/api/Address/${id}`)
 	return response
 }
 
